393be8c786ac47dc41c13d8bf7643efb52907f3a
brandon mazey <brandon@Mac.localdomain>
2026-02-02 10:11:11 -0500

    weave

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
         // System.out.println("new dawn, new day");
+        
+    }
 
-        // Emojis 🥀 🖤
-        
-        
+    public static void weave(String[][] rug) {
+        for(int i = 0; i < rug.length; i++) {
+            for(int j = 0; j < rug[i].length; j++) {
+                System.out.print(rug[i][j]);
+            }
+            System.out.println("");
+        }
     }
 }
