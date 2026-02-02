d723a893621b44d67e7e12eb091c45b5866a2a75
brandon mazey <brandon@brandons-MacBook-Air.local>
2026-02-02 07:35:56 -0500

    wrapping up notes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,21 +3,31 @@ import java.util.ArrayList;
 public class Hello {
     public static void main(String[] args) {
 
+        System.out.println(fork(5));
+
         // ArrayList demonstration.
-        ArrayList<String> jolibee = new ArrayList<String>();
-        jolibee.add("Honey BBQ Tenders 6pc");
-        jolibee.add("XL Lemonade");
-        jolibee.add("Spaghetti");
-        jolibee.add(1, "French Fries");
+        // ArrayList<String> jolibee = new ArrayList<String>();
+        // jolibee.add("Honey BBQ Tenders 6pc");
+        // jolibee.add("XL Lemonade");
+        // jolibee.add("Spaghetti");
+        // jolibee.add(1, "French Fries");
 
-        for(String item : jolibee) {
-            System.out.println(item);
+        // for(String item : jolibee) {
+        //     System.out.println(item);
+        // }
+
+        // if (jolibee.contains("Small Lemonade")) {
+        //     System.out.println("Small Lemonade is in the list!");
+        // } else {
+        //     System.out.println("Small Lemonade is NOT in the list!");
+        // }
+    }
+
+    public static int fork(int n) {
+        if (n <= 1) {
+            return 1;
         }
 
-        if (jolibee.contains("Small Lemonade")) {
-            System.out.println("Small Lemonade is in the list!");
-        } else {
-            System.out.println("Small Lemonade is NOT in the list!");
-        }
+        return fork(n - 1) + fork(n - 2);
     }
 }
